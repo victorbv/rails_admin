3249512aaabf9dca1d4fb522537afa12f9a6e76c
Victor <victor@lapisraro.com.br>
2011-08-13 12:04:07 -0300

    avoid saving history, while instability isn't solved (other lines)

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -162,7 +162,7 @@ module RailsAdmin
       @object.attributes = @attributes
 
       if @object.save
-        AbstractHistory.create_update_history @abstract_model, @object, @cached_assocations_hash, associations_hash, @modified_assoc, @old_object, _current_user
+        #AbstractHistory.create_update_history @abstract_model, @object, @cached_assocations_hash, associations_hash, @modified_assoc, @old_object, _current_user
         respond_to do |format|
           format.html do
             redirect_to_on_success
@@ -194,7 +194,7 @@ module RailsAdmin
       @authorization_adapter.authorize(:destroy, @abstract_model, @object) if @authorization_adapter
 
       if @object.destroy
-        AbstractHistory.create_history_item("Destroyed #{@model_config.with(:object => @object).object_label}", @object, @abstract_model, _current_user)
+        #AbstractHistory.create_history_item("Destroyed #{@model_config.with(:object => @object).object_label}", @object, @abstract_model, _current_user)
         flash[:notice] = t("admin.flash.successful", :name => @model_config.label, :action => t("admin.actions.deleted"))
       else
         flash[:error] = t("admin.flash.error", :name => @model_config.label, :action => t("admin.actions.deleted"))
@@ -256,7 +256,7 @@ module RailsAdmin
 
       destroyed.each do |object|
         message = "Destroyed #{@model_config.with(:object => object).object_label}"
-        AbstractHistory.create_history_item(message, object, @abstract_model, _current_user)
+        #AbstractHistory.create_history_item(message, object, @abstract_model, _current_user)
       end
 
       unless destroyed.empty?
